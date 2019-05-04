ce768fffe495519a1c3837a7c195a56cc7335a4b
matuzalema <matuzalema@gmail.com>
2019-05-04 11:43:14 +0200

    redux-devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,14 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import reducers from '../reducers/index';
+import DevTools from '../DevTools';
 
-const store = createStore(reducers);
+const store = createStore(
+  reducers,
+  DevTools.instrument()
+);
 
 export default store;
+
+
+
+
